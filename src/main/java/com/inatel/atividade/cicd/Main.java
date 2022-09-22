ff6fac72bfc8d82688982b216be202b58d10ac60
Uttoni Alfredo Brandani <ualfredo@uolinc.com>
2022-09-22 20:41:30 -0300

    Adicionando dependencia do junit e codigo inicial

---
+++ src/main/java/com/inatel/atividade/cicd/Main.java
@@ -0,0 +1,8 @@
+package com.inatel.atividade.cicd;
+
+public class Main {
+    public static void main(String[] args) {
+        Motivador motivador = new Motivador();
+        motivador.sendMessage();
+    }
+}
---
+++ src/main/java/com/inatel/atividade/cicd/Motivador.java
@@ -0,0 +1,8 @@
+package com.inatel.atividade.cicd;
+
+public class Motivador {
+
+    public void sendMessage(){
+        System.out.println("Porra ,que vida hein");
+    }
+}
