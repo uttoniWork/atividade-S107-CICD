5de08dc2b6e6ddd67ea4039bdab8559d05b599f8
Uttoni Alfredo Brandani <ualfredo@uolinc.com>
2022-09-22 21:48:03 -0300

    Adicionando script que envia email e step na pipeline

---
+++ script/email-script.sh
@@ -0,0 +1,8 @@
+echo "Instalando mailutils..."
+sudo apt-get install mailutils
+sudo "Instalação completa"
+
+email = $email
+
+echo "Mandando e-mail com o mail do linux" | mail -s "subject: Atividade CICD" email
+
