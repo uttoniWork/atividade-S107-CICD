8e6d79c5a369099bdeeac57818af0c1254b14597
Camila Silva <camila.silva@watchguard.com>
2022-09-25 18:30:32 -0300

    Mudando corpo do email

--- script/email-script.sh
+++ script/email-script.sh
@@ -7,5 +7,5 @@ echo "Instalação completa"
 EMAIL = $EMAIL
 
 echo $EMAIL
-echo "Mandando e-mail com o mail do linux" | mail -s "subject: Atividade CICD" "$EMAIL"
+echo "Pipeline executado com sucesso" | mail -s "subject: Atividade CICD" "$EMAIL"
 
