4fa0ed9eb47abe520f74fcdcce3398e4e2f615a9
Camila Silva <camila.silva@watchguard.com>
2022-09-25 17:25:48 -0300

    Mudanças nos arquivos yaml e shell

--- script/email-script.sh
+++ script/email-script.sh
@@ -1,8 +1,10 @@
+ls
+cd src
 echo "Instalando mailutils..."
 sudo apt-get install mailutils
 echo "Instalação completa"
 
-$email = $EMAIL
-echo $email
-echo "Mandando e-mail com o mail do linux" | mail -s "subject: Atividade CICD" email
+EMAIL = $EMAIL
+echo $EMAIL
+echo "Mandando e-mail com o mail do linux" | mail -s "subject: Atividade CICD" ${EMAIL}
 
