0479aec0146d17d5e41bba609c2370c8bf7f24ab
Uttoni Alfredo Brandani <ualfredo@uolinc.com>
2022-09-26 01:21:21 -0300

    Adicionando main

--- src/main/java/com/inatel/atividade/cicd/Main.java
+++ src/main/java/com/inatel/atividade/cicd/Main.java
@@ -1,8 +1,15 @@
 package com.inatel.atividade.cicd;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        final Motivador motivador = new Motivador();
-        motivador.sendMessage();
+
+        final SentimentoDiario sentimentoDiario = new SentimentoDiario();
+        final DayOfWeek diaDaSemana = LocalDate.now().getDayOfWeek();
+
+        System.out.println("Sentimento do dia: ");
+        System.out.println(sentimentoDiario.getSentimentoDoDia(diaDaSemana));
     }
 }
